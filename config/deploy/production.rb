9c9dae458deb1335eb93312834446cf4087e30fe
Elliot Nelson <nelson.work@gmail.com>
2009-10-12 11:45:50 -0400

    config updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@ set :hostname, 'fleet.gomoshi.com'
 set :deploy_to, "/home/sites/#{hostname}"
 
 # Roles
-#role :app, 'walter.gomoshi.com'
-#role :web, 'walter.gomoshi.com'
-#role :db,  'walter.gomoshi.com', :primary => true
+role :app, 'walter.gomoshi.com'
+role :web, 'walter.gomoshi.com'
+role :db,  'walter.gomoshi.com', :primary => true
 
 # Rails environment for migrations
 set :rails_env, 'production'
