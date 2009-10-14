f49677ead4a53b85d48f1c287ecdf77afe5a70f1
Chris Gaffney <cgaffney@crayoninterface.com>
2009-10-14 10:57:06 -0400

    Clean up comments about deprecated features. Ensure password is always filtered from logs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,10 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'f24be8301931c1bd9582a013e9b0f904'
+  protect_from_forgery
   
   # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
   
   protected
   def phone_software_path
