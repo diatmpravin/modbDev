0323db37480a5493f7a475f505d26a7bb98cb0e9
Chris Gaffney <cgaffney@crayoninterface.com>
2009-10-15 11:07:01 -0400

    Tweak concerned_with so it can be used in plain objects.

--- config/initializers/concerns.rb
+++ config/initializers/concerns.rb
@@ -1,7 +1,9 @@
-class << ActiveRecord::Base
+module Concerns
   def concerned_with(*concerns)
     concerns.each do |concern|
       require_dependency "#{name.underscore}/#{concern}"
     end
   end
 end
+
+ActiveRecord::Base.send(:extend, Concerns)
