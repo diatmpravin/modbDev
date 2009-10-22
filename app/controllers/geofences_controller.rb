3c8db448fada1095e5c2cfa95f22b5fe0723a925
Elliot Nelson <nelson.work@gmail.com>
2009-10-22 15:29:32 -0400

    dry up geofences controller (one new before_filter and push create+update logic into a single function)

--- app/controllers/geofences_controller.rb
+++ app/controllers/geofences_controller.rb
@@ -1,7 +1,8 @@
 class GeofencesController < ApplicationController
+  before_filter :new_geofence, :only => [:new, :create]
+  before_filter :set_geofence, :only => [:show, :edit, :update, :destroy]
   before_filter :set_device
   before_filter :set_devices
-  before_filter :set_geofence, :only => [:show, :edit, :update, :destroy]
   
   layout except_ajax('geofences')
   
@@ -23,13 +24,56 @@ class GeofencesController < ApplicationController
   end
   
   def new
-    @geofence = current_account.geofences.new
   end
   
   def create
-    @geofence = current_account.geofences.new
+    save_geofence(params[:geofence])
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    save_geofence(params[:geofence])
+  end
+  
+  def destroy
+    @geofence.destroy
     
-    if @geofence.update_attributes(params[:geofence])
+    respond_to do |format|
+      format.json {
+        render :json => {
+          :status => 'success'
+        }
+      }
+    end
+  end
+  
+  protected
+  def new_geofence
+    @geofence = current_account.geofences.new
+  end
+  
+  def set_geofence
+    @geofence = current_account.geofences.find(params[:id])
+  end
+  
+  def set_device
+    @device = current_account.devices.find(params[:device_id]) if params[:device_id]
+  end
+  
+  def set_devices
+    @devices = current_account.devices
+  end
+  
+  def save_geofence(record)
+    # If device ids or alert_recipient ids are missing, blank them out
+    record = {:device_ids => [], :alert_recipient_ids => []}.merge(record)
+    
+    if @geofence.update_attributes(record)
       respond_to do |format|
         format.json {
           render :json => {
@@ -44,53 +88,12 @@ class GeofencesController < ApplicationController
         format.json {
           render :json => {
             :status => 'failure',
-            :html => render_to_string(:action => 'new')
+            :html => render_to_string(
+              :action => @geofence.new_record? ? 'new' : 'edit'
+            )
           }
         }
       end
     end
-  end
-  
-  def show
-  end
-  
-  def edit
-  end
-  
-  def update
-    params[:geofence][:device_ids] ||= []
-    params[:geofence][:alert_recipient_ids] ||= []
-    
-    if @geofence.update_attributes(params[:geofence])
-      render :json => {
-        :status => 'success',
-        :view => render_to_string(:action => 'show'),
-        :edit => render_to_string(:action => 'edit')
-      }
-    else
-      render :json => {
-        :status => 'failure',
-        :html => render_to_string(:action => 'edit')
-      }
-    end
-  end
-  
-  def destroy
-    @geofence.destroy
-    
-    render :json => {:status => 'success'}
-  end
-  
-  protected
-  def set_device
-    @device = current_account.devices.find(params[:device_id]) if params[:device_id]
-  end
-  
-  def set_devices
-    @devices = current_account.devices
-  end
-  
-  def set_geofence
-    @geofence = current_account.geofences.find(params[:id])
   end
 end
