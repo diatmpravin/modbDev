30553258da955dc4afd17cf64e89b548d5b51bbe
Jason Roelofs <jameskilton@gmail.com>
2009-11-19 15:28:21 -0500

    Undid the account -> user change, as devices are on the account, not the user

--- app/models/account.rb
+++ app/models/account.rb
@@ -16,6 +16,12 @@ class Account < ActiveRecord::Base
   # List accessible attributes here
   attr_accessible :devices, :phones, :geofences, :alert_recipients, :tags, :today,
     :name, :reseller, :can_assign_reseller, :landmarks
+
+  # Work around bug:
+  # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/2896-collection_singular_ids-breaks-when-used-with-include
+  def device_ids
+    Device.find_by_sql(["select d.id from devices d where account_id = ?", self.id]).map(&:id)
+  end
   
   def setup?
     self.setup_status == 0
