87a4867bacc147a9a32fe2352b6507030e4945ce
Jason Roelofs <jameskilton@gmail.com>
2010-02-19 16:26:45 -0500

    Change to god from monit, but don't run because god's not running correctly yet

--- lib/recipes/resque.rb
+++ lib/recipes/resque.rb
@@ -1,20 +1,20 @@
 namespace :resque do
   desc "Stop the running workers"
   task :stop, :roles => :app do
-    sudo "/usr/bin/env monit -g fleet_workers stop"
+    sudo "/usr/bin/env god stop fleet_workers"
   end
 
   desc "Start the running workers"
   task :start, :roles => :app do
-    sudo "/usr/bin/env monit -g fleet_workers start"
+    sudo "/usr/bin/env god start fleet_workers"
   end
 
   desc "Restart the running workers"
   task :restart, :roles => :app do
-    sudo "/usr/bin/env monit -g fleet_workers restart"
+    sudo "/usr/bin/env god restart fleet_workers"
   end
 end
 
-before 'deploy:stop',    'resque:stop'
-before 'deploy:start',   'resque:start'
-after  'deploy:restart', 'resque:restart'
+#before 'deploy:stop',    'resque:stop'
+#before 'deploy:start',   'resque:start'
+#after  'deploy:restart', 'resque:restart'
