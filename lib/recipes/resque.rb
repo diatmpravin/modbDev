3d96ec9685e309746c853bc90eb3e6c22ca705ea
Jason Roelofs <jameskilton@gmail.com>
2010-02-22 09:53:03 -0500

    Re-enable worker restart recipes, fix group name

--- lib/recipes/resque.rb
+++ lib/recipes/resque.rb
@@ -1,20 +1,20 @@
 namespace :resque do
   desc "Stop the running workers"
   task :stop, :roles => :app do
-    sudo "/usr/bin/env god stop fleet_workers"
+    sudo "/usr/bin/env god stop fleet-workers"
   end
 
   desc "Start the running workers"
   task :start, :roles => :app do
-    sudo "/usr/bin/env god start fleet_workers"
+    sudo "/usr/bin/env god start fleet-workers"
   end
 
   desc "Restart the running workers"
   task :restart, :roles => :app do
-    sudo "/usr/bin/env god restart fleet_workers"
+    sudo "/usr/bin/env god restart fleet-workers"
   end
 end
 
-#before 'deploy:stop',    'resque:stop'
-#before 'deploy:start',   'resque:start'
-#after  'deploy:restart', 'resque:restart'
+before 'deploy:stop',    'resque:stop'
+before 'deploy:start',   'resque:start'
+after  'deploy:restart', 'resque:restart'
