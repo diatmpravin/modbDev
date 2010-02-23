c3afa7d3bee259d9dd8926939c69cbea8cf03458
Jason Roelofs <jameskilton@gmail.com>
2010-02-23 15:32:18 -0500

    Start work on vehicles import process

--- app/controllers/import/vehicles_controller.rb
+++ app/controllers/import/vehicles_controller.rb
@@ -1,3 +1,22 @@
 class Import::VehiclesController < ApplicationController
+  before_filter :require_role
+
   layout except_ajax('devices')
+
+  # GET /import/vehicles
+  def index
+  end
+
+  # POST /import/vehicles
+  # Takes an uploaded file named :upload and attempts
+  # to parse it into usable vehicle information
+  def create
+  end
+
+  protected
+
+  def require_role
+    redirect_to root_path unless current_user.has_role?(User::Role::FLEET)
+  end
+
 end
--- test/functional/import/vehicles_controller_test.rb
+++ test/functional/import/vehicles_controller_test.rb
@@ -1,8 +1,46 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
-describe "Import::VehiclesController", ActionController::TestCase do
-  # Replace this with your real tests.
-  it "should be true" do
-    assert true
+context "Import::VehiclesController", ActionController::TestCase do
+  use_controller Import::VehiclesController
+
+  context "Main Page" do
+
+    setup do
+      @user = users(:quentin)
+      login_as :quentin
+    end
+
+    specify "shows upload form" do
+      get :index
+      template.should.be "index"
+    end
+
+    specify "requires fleet access" do
+      @user.update_attributes(:roles => [User::Role::USERS])
+
+      get :index
+      should.redirect_to root_path
+    end
+
+  end
+
+  context "Uploading file" do
+
+    context "Allows certain types" do
+
+      xspecify "allows xls"
+
+      xspecify "allows csv"
+
+      xspecify "allows ods" 
+
+      xspecify "errors out if file type not recognized"
+
+    end
+
+    context "Processes file" do
+
+    end
+
   end
 end
