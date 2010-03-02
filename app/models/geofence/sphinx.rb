35ed862eed72ff0b259645b58fab18f9f76b46c3
tbrack <tbrack@crayoninterface.com>
2010-03-02 17:22:36 -0500

    index geofences by device group names

--- app/models/geofence/sphinx.rb
+++ app/models/geofence/sphinx.rb
@@ -5,6 +5,7 @@ class Geofence < ActiveRecord::Base
   ##
   define_index do
     indexes :name, :sortable => true
+    indexes device_groups.name, :as => :group_name
 
     has :account_id, :created_at, :updated_at
 
