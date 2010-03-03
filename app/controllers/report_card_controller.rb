48939457b32283096eae9e6fa78344eb06ef91ec
Jason Roelofs <jameskilton@gmail.com>
2010-03-03 15:53:59 -0500

    Now aggregating data throw group descendants and all related vehicles

--- app/controllers/report_card_controller.rb
+++ app/controllers/report_card_controller.rb
@@ -5,6 +5,9 @@ class ReportCardController < ApplicationController
   # GET /report_card
   # Main view of the report card
   def show
+    @report_card = current_account.groups.of_devices.roots.map do |g|
+      GroupSummaryReport.new(current_user, :group => g, :range => {:type => 1}).tap {|g| g.run }
+    end
   end
 
 end
---
+++ app/models/report/group_summary_report.rb
@@ -0,0 +1,90 @@
+# This report is basically identical to the VehicleSummaryReport, but instead
+# it aggregates the summary across all vehicles in a group and this group's 
+# sub-groups
+class GroupSummaryReport < Report
+
+  def initialize(user, options = {})
+    @group = options.delete(:group)
+
+    # We do our own device handling
+    super(user, options.merge(:devices => []))
+  end
+
+  def title
+    "Group Summary Report - Group #{@group.name}: #{self.start} through #{self.end}"
+  end
+
+  def to_csv
+    self.data.rename_columns(
+      :name => "Group Name",
+      :duration => "Operating Time (s)",
+      :miles => "Miles",
+      :event_speed => "Speed Events",
+      :event_geofence => "Geofence Events",
+      :event_idle => "Idle Events",
+      :event_aggressive => "Aggressive Events",
+      :event_after_hours => "After Hours Events"
+    )
+    super
+  end
+
+  def run
+    self.data = Ruport::Data::Table(
+      :name,
+      :duration,
+      :miles,
+      :event_speed,
+      :event_geofence,
+      :event_idle,
+      :event_aggressive,
+      :event_after_hours
+    )
+
+    aggregate = {
+      :first_start_time => [],
+      :duration => 0,
+      :miles => 0,
+      :event_speed => 0,
+      :event_geofence => 0,
+      :event_idle => 0,
+      :event_aggressive => 0,
+      :event_after_hours => 0
+    }
+
+    @group.self_and_descendants.each do |g|
+      g.devices.each do |device|
+        trips = device.trips.in_range(self.start, self.end, self.user.zone)
+
+        # Do event grouping in database
+        events = device.events.in_range(self.start, self.end, self.user.zone).all(
+          :select => 'event_type, COUNT(*) AS count_all',
+          :group => :event_type
+        ).map {|e| [e.event_type, e.count_all.to_i]}
+        
+        # Hashify
+        events = Hash[*events.inject([]) {|arr, elem| arr.concat(elem)}]
+
+        aggregate[:first_start_time] << trips.first.start if trips.any?
+        aggregate[:duration] += trips.map {|t| t.duration}.sum
+        aggregate[:miles] += trips.map {|t| t.miles}.sum
+        aggregate[:event_speed] += events[Event::SPEED] || 0
+        aggregate[:event_geofence] += [
+            events[Event::ENTER_BOUNDARY] || 0,
+            events[Event::EXIT_BOUNDARY] || 0
+          ].sum
+        aggregate[:event_idle] += events[Event::IDLE] || 0
+        aggregate[:event_aggressive] += [
+            events[Event::RPM] || 0,
+            events[Event::RAPID_ACCEL] || 0,
+            events[Event::RAPID_DECEL] || 0
+          ].sum
+        aggregate[:event_after_hours] += events[Event::AFTER_HOURS] || 0
+      end
+    end
+
+    aggregate[:first_start_time] = aggregate[:first_start_time].sort.first
+
+    self.data = aggregate.merge(:name => @group.name)
+  end
+
+end
