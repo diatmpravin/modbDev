f96db8650304ddf01d93e4237a4a8057eeb0dcbc
Elliot Nelson <nelson.work@gmail.com>
2010-03-05 13:19:32 -0500

    form now submits groups and saves them

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,13 +1,31 @@
 module GroupsHelper
 
-  # TODO (When Necessary): Improve performance by removing recursion
+  # Create a group tree that allows the user to select a list of groups. Intended
+  # to be used in conjunction with the ".groupSelect()" jQuery function.
+  #
+  # Options:
+  #
+  #   :checkbox  a string to be used as the field name of the checkboxes. If
+  #              not included, checkboxes will be visible to the user, but not
+  #              result in any form action on submit.
+  #
+  #   :selected  a list of groups already "selected" on the object you're
+  #              working with. Leave this option out to start with no selected
+  #              groups.
+  #
   def group_tree(groups, options = {})
+    options[:selected] ||= []
+    options[:selected_ids] ||= options[:selected].map(&:id) 
+    
     content_tag(:ol, groups.map { |g|
-      content_tag(:li, "#{content_tag(:span, '', :class => 'checkbox')}
-      #{content_tag(:span, '', :class => 'collapsible closed')}
-      #{content_tag(:span, g.name, :class => 'name')}
-      #{group_tree(g.children, options.merge(:style => 'display:none')) if g.children.any?}")
-    }, options)
+      content_tag(:li, [
+        options[:checkbox] ? check_box_tag(options[:checkbox], g.id, options[:selected_ids].include?(g.id)) : nil,
+        content_tag(:span, '', :class => 'checkbox'),
+        content_tag(:span, '', :class => 'collapsible closed'),
+        content_tag(:span, g.name, :class => 'name'),
+        g.children.any? ? group_tree(g.children, options.merge(:style => 'display:none')) : nil
+      ].join)
+    }, {:style => options[:style]})
   end
   
 end
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -223,21 +223,26 @@ jQuery.fn.fitWindow = function(callback) {
  * This method doesn't handle the look (stylesheet will take care of that).
  */
 jQuery.fn.groupSelect = function() {
+  // Check/uncheck rows when clicked
   q(this).find('li').click(function() {
     var self = q(this);
     
     if (self.hasClass('checked')) {
       self.find('li').andSelf().removeClass('checked halfchecked');
+      self.find('input').attr('checked', false);
     } else {
       self.find('li').andSelf().removeClass('halfchecked').addClass('checked');
+      self.find('input').attr('checked', true);
     }
     
     self.parents('li').removeClass('halfchecked checked')
+        .children('input').attr('checked', false).end()
         .filter(':has(li.checked)').addClass('halfchecked');
     
     return false;
   });
   
+  // Collapse and expand group lists when arrow is clicked
   q(this).find('span.collapsible').click(function() {
     q(this).toggleClass('open').toggleClass('closed');
     q(this).siblings('ol').toggle(q(this).hasClass('open'));
@@ -245,6 +250,15 @@ jQuery.fn.groupSelect = function() {
     return false;
   });
   
+  // Hide the collapse/expand arrow for leaf rows
   q(this).find('li:not(:has(li)) span.collapsible').hide();
-  //q(this).find('span.collapsible:visible').addClass('open');
+  
+  // Check all rows that should start as selected
+  q(this).find('input:checked')
+         .parent().addClass('checked')
+         .find('li').addClass('checked').end()
+         .parents('li:not(.checked)').addClass('halfchecked');
+  
+  // "Open" the tree for all pre-selected rows, to save time
+  q(this).find('li:has(li.checked) > span.collapsible').click();
 };
