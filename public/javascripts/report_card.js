7d94af7d6966421e6e42a346b85769d754c71673
Elliot Nelson <nelson.work@gmail.com>
2010-03-24 18:24:00 -0400

    Rewrite a large chunk of Report Card javascript, get group edit form & cancel working (but not saving), and fix the show/hide data pane.

--- public/javascripts/report_card.js
+++ public/javascripts/report_card.js
@@ -1,164 +1,138 @@
 /**
- * Handling for the Report Card page
+ * Report Card
  */
 if (typeof(ReportCard) == 'undefined') {
   ReportCard = {};
 }
 
-ReportCardGroup = {
+/**
+ * Report Card Frame
+ */
+ReportCard.Frame = {
+  init: function() {
+    // When browser window resizes, adjust the size of the report card frame
+    q('#frame').fitWindow(function(width, height) {
+      q('#frame').height(height - 32 - 2);
+    });
+    
+    // Adjust the padding of certain elements to reflect the scrollbar size
+    var scrollbarSize = q('#data_pane').width() - q('#data_pane > ol').width();
+    q('.scrollbar-fix').css('padding-right', function() {
+      return parseInt(q(this).css('padding-right')) + scrollbarSize;
+    });
+    
+    ReportCard.DataPane.init();
+  }
+};
+
+/**
+ * Report Card Data Pane
+ */
+ReportCard.DataPane = {
+  /**
+   * Initialize the Report Card "data pane", which contains the group tree and
+   * the report card table.
+   */
+  init: function() {
+    // Startup: hide collapsible arrows for empty groups
+    q('li:not(:has(li)) span.collapsible').hide();
+  
+    // Allow user to toggle collapsible groups open and closed
+    q('span.collapsible').live('click', function() {
+      var self = q(this);
+      if (self.closest('li').children('ol').toggle().css('display') == 'none') {
+        self.addClass('closed');
+      } else {
+        self.removeClass('closed');
+      }
+    });
+    
+    // Allow user to edit groups
+    q('div.group a.edit').live('click', ReportCard.Group.edit);
+  },
+  
+  /**
+   * Collapse the data pane, hiding the report card table and showing only
+   * the group tree on the left. This allows other panes to be visible on the
+   * right.
+   */
+  close: function() {
+    // "Fix" the width of the report card table so it doesn't resize
+    q('#data_pane > ol').css('width', function() { return q(this).width() + 'px'; });
+    
+    // Collapse the data pane
+    q('#data_pane').animate({width:280}, {duration:'fast'});
+    
+    return this;
+  },
+  
+  /**
+   * Open the data pane, hiding any existing panes and showing the report card
+   * table.
+   */
+  open: function() {
+    // Open the data pane
+    q('#data_pane').animate({width:'100%'}, {duration:'fast'});
+    
+    // "Unfix" the width of the report card table so it can be resized
+    q('#data_pane > ol').css('width', 'auto');
+    
+    return this;
+  },
+  
+  /**
+   * Display an alternate titlebar for the right pane. Calling with null or
+   * no parameters will reset the bar to the normal report card display.
+   */
+  title: function(newTitle) {
+    if (typeof(newTitle) == 'undefined' || newTitle == null) {
+      q('#report_card_header div.title').hide('fast');
+      q('#report_card_header div.data').show('fast');
+    } else {
+      q('#report_card_header div.title span').html(newTitle);
+      q('#report_card_header div.data').hide('fast');
+      q('#report_card_header div.title').show('fast');
+    }
+    
+    return this;
+  }
+};
+
+/**
+ * Report Card Group
+ */
+ReportCard.Group = {
+  /**
+   * Show the edit form for the selected group.
+   */
   edit: function() {
-    q('#data_pane').animate({width:281}, {duration: 'fast'});
-    q('#edit_pane').html('Hello, hello, hello?');
+    q('#edit_pane').find('.edit').hide().end()
+                   .find('.loading').show();
+    
+    ReportCard.DataPane.close().title('Edit Group');
+    
+    q.get(q(this).attr('href'), function(html) {
+      q('#edit_pane').find('.loading').hide().end()
+                     .find('.edit').html(html).show();
+      
+      q('#edit_pane a.cancel').click(ReportCard.Group.cancel);
+    });
+    
+    return false;
+  },
+  
+  /**
+   * Close the group edit form without saving.
+   */
+  cancel: function() {
+    ReportCard.DataPane.open().title();
+    
+    q('#edit_pane').find('.edit').hide().empty().end()
+                   .find('.loading').show();
+                    
     return false;
   }
 };
- 
-(function($) {
 
-  ReportCard = {
-    init: function() {
-      $('#frame').fitWindow(function(width, height) {
-        $('#frame').height(height - 32 - 2);
-      });
-      
-      /*q('#data_pane').scroll(function(e) {
-        
-        var desired = Math.floor(q(this).scrollTop() / 25) * 25;
-        if (desired != q(this).scrollTop()) {
-          q(this).stop(true).animate({scrollTop: desired}, {duration: 'fast', queue: false});
-        }
-      });*/
-      
-      q('div.row').hover(function() {
-        if (true) {
-          q(this).addClass('hover');
-        }
-      }, function() {
-        q(this).removeClass('hover');
-      });
-      
-      q('div.group span.collapsible').live('click', function() {
-        var self = q(this);
-        if (self.closest('li').children('ol').toggle().css('display') == 'none') {
-          self.addClass('closed');
-        } else {
-          self.removeClass('closed');
-        }
-      });
-      
-      // Hide the collapse/expand arrow for leaf rows
-      q(this).find('li:not(:has(li)) div.group span.collapsible').hide();
-      
-      // Scrollbar fix?
-      q('.scrollbar-fix').css('padding-right', function() {
-        return parseInt(q(this).css('padding-right')) + 17;
-      });
-      
-      q('div.group a.edit').live('click', ReportCardGroup.edit);
-      
-    }
-    ,
-    toggleGroup: function() {
-      var href = $(this).attr("href"),
-          row = $(this).parents("tr"),
-          parentId = row.attr("id");
-
-      if(!row.data("expanded")) {
-        row.find("span").hide();
-
-        var showRows = function() {
-          ReportCard.showNested(row);
-          row.data("expanded", true);
-          row.find(".collapse").show();
-          row.addClass("active");
-        };
-
-        if(!row.data("filled")) {
-          ReportCard.requestSubgroupData(href, row, showRows);
-        } else {
-          showRows();
-        }
-
-      } else {
-        ReportCard.hideNested(row);
-
-        row.data("expanded", false);
-        row.removeClass("active");
-        row.find("span").hide().end().find(".expand").show();
-      }
-
-      return false;
-    }
-    ,
-    /**
-     * Recursively show groups
-     */
-    showNested: function(parent) {
-      var parentId = parent.attr("id");
-
-      $.each(parent.siblings("tr." + parentId), function(idx, tr) {
-        tr = $(tr);
-
-        if(tr.data("expanded") == true) {
-          ReportCard.showNested(tr);
-        }
-
-        tr.show();
-      });
-    }
-    ,
-    /**
-     * Recursively hide nested entries
-     */
-    hideNested: function(parent) {
-      var parentId = parent.attr("id");
-      if(parentId == "") { return; }
-
-      $.each(parent.siblings("tr." + parentId + ":visible"), function(idx, tr) {
-        tr = $(tr);
-
-        ReportCard.hideNested(tr);
-
-        tr.hide();
-      });
-    }
-    ,
-    /**
-     * In the case that no sub data has been requested for the
-     * selected group, we need to request the data from the server
-     */
-    requestSubgroupData: function(href, row, callback) {
-      var range_type = $("#range_type").val();
-      if(!row.data("running")) {
-        $.ajax({
-          url: href + "&range_type=" + range_type,
-          beforeSend: function() {
-            row.data("running", true);
-
-            row.find("span").hide();
-            row.find(".busy").show();
-          },
-          complete: function() {
-            row.data("running", false);
-            row.find(".busy").hide();
-          },
-          error: function() {
-            row.find(".expand").show();
-          },
-          success: function(html) {
-            row.after(html);
-            row.data("filled", true);
-
-            callback();
-          }
-        });
-      }
-    }
-  };
-
-  // Hooking up live events
-  $("a.toggleGroup").live("click", ReportCard.toggleGroup);
-
-  // Document ready
-  $(ReportCard.init);
-})(jQuery);
+/* Initialize */
+jQuery(ReportCard.Frame.init);
