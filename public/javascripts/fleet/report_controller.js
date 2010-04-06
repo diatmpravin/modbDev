9f188546f102ea6d9099e9e7d129ed96003a0268
Travis Brack <tbrack@crayoninterface.com>
2010-04-06 13:46:29 -0400

    run reports with set list of ids

--- public/javascripts/fleet/report_controller.js
+++ public/javascripts/fleet/report_controller.js
@@ -5,12 +5,14 @@
  */
 var Fleet = Fleet || {};
 Fleet.ReportController = (function(ReportController, ReportPane, VehiclePane, $) {
-  var vehicles = null;
-  //    lookup = null;
+  var vehicles = null,
+      report_form;
   
   /**
    * init()
    */
+  ReportController.init = function () {
+  };
    
   
   /**
@@ -20,65 +22,52 @@ Fleet.ReportController = (function(ReportController, ReportPane, VehiclePane, $)
    * actions on the controller (most likely by the Frame).
    */
   ReportController.index = function() {
-    VehiclePane.init().open();
-    ReportPane.init().open();
-    
+    VehiclePane.init();
+    ReportPane.init();
+  
+    report_form = $('#runReportForm')
     $.getJSON('/reports', function(json) {
       ReportPane.initPane(json.html);
     });
 
+    VehiclePane.open();
+    ReportPane.open();
     //ReportController.refresh();
   };
   
-  //TODO: needs to get vehicles for the VehiclePane.
-  /**
-   * refresh()
-   *
-   * Load all landmarks from the server and populate them.
-   */
-  //ReportController.refresh = function() {
-  //  landmarks = lookup = null;
-  //  
-  //  $.getJSON('/landmarks.json', function(json) {
-  //    var idx, num, html;
-  //    
-  //    landmarks = json;
-  //    
-  //    for(idx = 0, num = landmarks.length, lookup = {}; idx < num; idx++) {
-  //      lookup[landmarks[idx].id] = landmarks[idx];
-  //    }
-  //    
-  //    LandmarkPane.showLandmarks(landmarks);
-  //    showLandmarksOnMap(landmarks);
-  //  });
-  //};
-
   /**
    * Create a report via ajax, displaying it in a new window if successful.
    */
-  //ReportPane.createReport = function() {
-  //  var _form = $(this).closest('form');
-  //  var _report = _form.data('report_form');
-  //  
-  //  // Copy the selection list into the form
-  //  _form.find(_report.deviceField).val(_report.getSelection());
-  //  _report.container.errors();
-  //  
-  //  _form.ajaxSubmit({
-  //    dataType: 'json',
-  //    beforeSubmit: function() { _form.find('.loading').show(); },
-  //    complete: function() { _form.find('.loading').hide(); },
-  //    success: function(json) {
-  //      if (json.status == 'success') {
-  //        window.open('/reports/' + json.report_id + '.html');
-  //      } else {
-  //        _report.container.errors(json.errors);
-  //      }
-  //    }
-  //  });
-  //  
-  //  return false;
-  //}
+  ReportController.createReport = function() {
+    var _form = report_form.find('form');
+    
+    // Copy the device selection list into the form
+    _form.find('input[name=device_ids]').val(ReportController.getSelection());
+    report_form.errors();
+    
+    _form.ajaxSubmit({
+      dataType: 'json',
+      //TODO: show loading - beforeSubmit: function() { _form.find('.loading').show(); },
+      //TOTO: end loading - complete: function() { _form.find('.loading').hide(); },
+      success: function(json) {
+        if (json.status == 'success') {
+          window.open('/reports/' + json.report_id + '.html');
+        } else {
+          report_form.errors(json.errors);
+        }
+      }
+    });
+    
+    return false;
+  };
+
+  /**
+   * Get the selected vehicle ids
+   */
+  ReportController.getSelection = function() {
+    //TODO: get the values from the VehiclesPane
+    return [12,22,40]; 
+  };
  
   return ReportController;
 }(Fleet.ReportController || {}, Fleet.Frame.ReportPane, Fleet.Frame.VehiclePane, jQuery));
