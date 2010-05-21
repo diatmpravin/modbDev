989fc13e4a6a9e1f0fe077369d98cc35ed216aba
Travis Brack <tbrack@crayoninterface.com>
2010-05-21 17:07:48 -0400

    ancestor uncheck

--- public/javascripts/fleet/frame/vehicle_pane.js
+++ public/javascripts/fleet/frame/vehicle_pane.js
@@ -78,6 +78,11 @@ Fleet.Frame.VehiclePane = (function(VehiclePane, Fleet, $) {
 
       // if it is a group, set all children similarly.
       row.siblings('ol').find('div.row').toggleClass('active', row.hasClass('active'));
+
+      // if unselecting, unselect any ancestor that is selected
+      if (!row.hasClass('active')) {
+        row.parents('li').children('div.row').removeClass('active');
+      }
       
       return false;
   };
