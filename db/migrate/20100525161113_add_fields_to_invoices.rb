ca8ca0dcd8d644cdc1d855585a3f1db27eb62cc1
Travis Brack <tbrack@crayoninterface.com>
2010-05-25 15:10:24 -0400

    added a few more fields to invoices

---
+++ db/migrate/20100525161113_add_fields_to_invoices.rb
@@ -0,0 +1,13 @@
+class AddFieldsToInvoices < ActiveRecord::Migration
+  def self.up
+    add_column :invoices, :number, :integer
+    add_column :invoices, :generated_on, :date
+    add_column :invoices, :period_start, :date
+  end
+
+  def self.down
+    remove_column :invoices, :number
+    remove_column :invoices, :generated_on
+    remove_column :Invoices, :period_start
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100524152134) do
+ActiveRecord::Schema.define(:version => 20100525161113) do
 
   create_table "accounts", :force => true do |t|
     t.datetime "created_at"
@@ -193,6 +193,9 @@ ActiveRecord::Schema.define(:version => 20100524152134) do
     t.boolean  "paid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "number"
+    t.date     "generated_on"
+    t.date     "period_start"
   end
 
   create_table "landmarks", :force => true do |t|
